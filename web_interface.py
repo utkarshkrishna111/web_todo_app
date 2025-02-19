8df748c490b704a4e0d7b753fba7d8a3c00ae6f7
Utkarsh Krishna <utkarsh_krishna@outlook.com>
2025-02-19 22:59:36 +0100

    Added new feature Web Based todo app

--- web_interface.py
+++ web_interface.py
@@ -1,12 +1,14 @@
 """To run the script on local streamlit run <.py> directly on command line
 Title function return a title instance
 A callback function is a custom function
-st.session_state: gives values of active session variables"""
+st.session_state: gives values of active session variables
+unsafe_allow_html is used to enable html indicator identification in write line"""
 
 import streamlit as st
 import function_call
 
 todos = function_call.get_todos()
+st.set_page_config(layout="wide")
 
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
@@ -17,7 +19,11 @@ st.title("My Todo App")
 
 st.subheader("This is my Todo App")
 
-st.write("This app is to increase your productivity")
+st.write("This app is to increase your <b>productivity</b>",
+         unsafe_allow_html=True)
+
+st.text_input(label="", placeholder= "Enter a Todo...",
+              on_change=add_todo, key= 'new_todo')
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo,key=todo)
@@ -27,8 +33,7 @@ for index, todo in enumerate(todos):
         del  st.session_state[todo]
         st.rerun()
 
-st.text_input(label="", placeholder= "Enter a Todo...",
-              on_change=add_todo, key= 'new_todo')
+
 
 
 
